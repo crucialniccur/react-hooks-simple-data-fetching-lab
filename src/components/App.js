6c3eadd62f9c972882f05547c1fa322deed8990b
Mati Joseph <crucialniccur@gmail.com>
2025-04-15 06:36:22 +0300

    add a useState and define the useEffect with the fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 // create your App component here
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  });
+
   return (
     <div>
       <h2>Wagwan wadau</h2>
-      <img src={here} alt="A Random Dog" />
+      <img src={imageUrl} alt="A Random Dog" />
     </div>
   );
 }
